12339799797f1d4e6bc1444cf9ef78e9b7079db5
Maksym Dziuba <dziuba200517@gmail.com>
2025-11-11 19:31:07 +0100

    Initialized empty frontend and backend

---
+++ apg-backend/src/main/java/com/maximdziuba/apg_backend/ApgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maximdziuba.apg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgBackendApplication.class, args);
+	}
+
+}
---
+++ apg-backend/src/test/java/com/maximdziuba/apg_backend/ApgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximdziuba.apg_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
